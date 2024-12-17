832d7d78a7d4021d23d9c1629aa96ae63504f357
KP17893 <krishpathak23725@gmail.com>
2024-12-17 15:24:51 +0530

    webstorm

---
+++ prac set 1/test1.js
@@ -0,0 +1,26 @@
+//Q1
+let a="skillop";
+a+=1;
+console.log(a+" "+25);
+
+//Q2
+console.log(typeof (a));
+
+//Q3 and Q4
+// const a={
+//     name:"ganji",
+//     id:34
+// }
+//a=45;
+//a['class']=4
+
+//Q5
+const dictionary={
+    procastination:"to postpone the task for tomorrow",
+    allusion:"reference to something that avoids mentioning the thing directly",
+    cavalier:"no concern for important or serious matters"
+}
+console.log(dictionary);
+
+
+
---
+++ prac set 2/test1.js
@@ -0,0 +1,12 @@
+//Q1
+
+
+let a=[11,45];
+for (let i = 0; i < a.length; i++) {
+    if(a[i]>10 && a[i]<20){
+        console.log("you are in range");
+    }
+    else{
+        console.log("sorry, get lost");
+    }
+}
