095f539ff1c2659d76aa6ef9618abe867bdc3086
KP17893 <krishpathak23725@gmail.com>
2024-12-24 12:00:59 +0530

    till ex 1

--- exc1/index.js
+++ exc1/index.js
@@ -1,13 +1,14 @@
 
 function getInt(){
 
-    let num=Math.floor(Math.random()*101)
+    let num=Math.floor(Math.random()*11)
     return num;
 
 }
 while(1){
-    let a=parseInt(prompt("What is your number?"));
-    if(a==getInt()){
+    let a=parseInt(prompt("What is your number between 1 to 10 ?"));
+    let cc=getInt();
+    if(a===cc){
         alert("correct");
     }
     else{
